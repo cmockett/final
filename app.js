a49f8d802b6e1a67ea2bf9c7ab5c618a559f8768
Student <student@RU-109.local>
2015-10-28 17:22:19 -0600

    5th, core functions working

--- app.js
+++ app.js
@@ -9,80 +9,104 @@ var earTrainingFunction = function($scope, $index){
 
 	$scope.keysArray = [
 		{
-			key: "A3",
+			key   : "A3",
+			label : "A"
 		},
 		{
-			key: "A#3",
+			key   : "A#3",
+			label : "A#"
 		},
 		{
-			key: "B3",
+			key   : "B3",
+			label : "B"
 		},
 		{
-			key: "C4",
+			key   : "C4",
+			label : "C"
 		},
 		{
-			key: "C#4",
+			key   : "C#4",
+			label : "C#"
 		},
 		{
-			key: "D4",
+			key   : "D4",
+			label : "D"
 		},
 		{
-			key: "D#4",
+			key   : "D#4",
+			label : "D#"
 		},
 		{
-			key: "E4",
+			key   : "E4",
+			label : "E"
 		},
 		{
-			key: "F4",
+			key   : "F4",
+			label : "F"
 		},
 		{
-			key: "F#4",
+			key   : "F#4",
+			label : "F#"
 		},
 		{
-			key: "G4",
+			key   : "G4",
+			label : "G"
 		},
 		{
-			key: "G#4",
+			key   : "G#4",
+			label : "G#"
 		},
 		{
-			key: "A4",
+			key   : "A4",
+			label : "A"
 		},
 		{
-			key: "A#4",
+			key   : "A#4",
+			label : "A#"
 		},
 		{
-			key: "B4",
+			key   : "B4",
+			label : "B"
 		},
 		{
-			key: "C5",
+			key   : "C5",
+			label : "C"
 		},
 		{
-			key: "C#5",
+			key   : "C#5",
+			label : "C#"
 		},
 		{
-			key: "D5",
+			key   : "D5",
+			label : "D"
 		},
 		{
-			key: "D#5",
+			key   : "D#5",
+			label : "D#"
 		},
 		{
-			key: "E5",
+			key   : "E5",
+			label : "E"
 		},
 		{
-			key: "F5",
+			key   : "F5",
+			label : "F"
 		},
 		{
-			key: "F#5",
+			key   : "F#5",
+			label : "F#"
 		},
 		{
-			key: "G5",
+			key   : "G5",
+			label : "G"
 		},
 		{
-			key: "G#5",
+			key   : "G#5",
+			label : "G#"
 		},
 	]
 
-		$scope.scalesArray = [
+	$scope.scalesArray = [
 		{
 			scaleName: "Chromatic",
 			scalePattern: [1,1,1,1,1,1,1,1,1,1,1,1],
@@ -155,10 +179,13 @@ var earTrainingFunction = function($scope, $index){
 			intervalPattern: ["1","2","b3","4","5","6","7"],
 			scaleNotes : []
 		}
-		]
+	]
 
 		$scope.notes = []
+		$scope.notesNoNumber = []
+
 		$scope.setKeyScale = function(){
+			$scope.notesNoNumber.length =0
 			$scope.buttonsDivHide = true
 			$scope.keyIndex
 			$scope.compareArray.length = 0
@@ -177,7 +204,17 @@ var earTrainingFunction = function($scope, $index){
 			$scope.compareArray.filter(function(el){
 				$scope.notes.push(el.key);
 			})
-			$scope.scalesArray[$scope.selectScale].scaleNotes.push($scope.notes)
+
+			//Removing # from each Note Value, just for display on the ng-repeated buttons
+			for(var i = 0; i<$scope.notes.length; i++){
+				var tempArray=[]
+				var tempVar = ''
+				tempArray = $scope.notes[i].split("")
+				tempArray.splice(-1, 1)
+				tempVar = tempArray.join('')
+				$scope.notesNoNumber.push(tempVar)
+			}
+			// $scope.scalesArray[$scope.selectScale].scaleNotes.push($scope.notes)
 
 			// console.log($scope.scalesArray[$scope.selectScale].scaleNotes)
 			// console.log($scope.scalesArray[$scope.selectScale].intervalPattern)
@@ -188,10 +225,11 @@ var earTrainingFunction = function($scope, $index){
 
 		$scope.timbreArray = ['Sine', 'Square', 'Sawtooth', 'Triangle']
 
+		$scope.numNotes
+
 		$scope.startNotesTest = function(){
 			$scope.notesArray.length = 0
-			$scope.numNotes
-			console.log($scope.numNotes)
+			$scope.playedNotesArray.length = 0
 			$scope.selectTimbreLC = $scope.selectTimbre.toLowerCase()
 			var timbre = new Wad({
 				source : $scope.selectTimbreLC
@@ -451,7 +489,7 @@ var earTrainingFunction = function($scope, $index){
 			
 			console.log($scope.notesArray)
 		}
-
+		$scope.playedNotesArray = []
 		$scope.playNote = function($index){
 			$scope.selectTimbreLC = $scope.selectTimbre.toLowerCase()
 			
@@ -467,12 +505,304 @@ var earTrainingFunction = function($scope, $index){
 				panning : 0,
 				env     : {hold : 1},
 				});
+
+			$scope.playedNotesArray.push($scope.notes[$index])
+			console.log($scope.playedNotesArray)
+
+			if($scope.numNotes == $scope.playedNotesArray.length){
+				if($scope.notesArray.join() === $scope.playedNotesArray.join()){
+					alert("You win!")
+				}
+				else{
+					alert("You suck!")
+				}
+			}
 		}
+		$scope.randomIndex
+		$scope.playScale = function(){
+			$scope.randomIndex = Math.floor($scope.scalesArray.length * Math.random())
+			console.log($scope.scalesArray[$scope.randomIndex].scalePattern)
+			$scope.keyIndex
+			$scope.compareArray.length = 0
+			$scope.notes.length = 0
 
+			for(var i=0; i<($scope.keysArray.length/2); i++){
+				if($scope.keysArray[i].key === $scope.selectKeys){
+					$scope.keyIndex = i
+				}
+			}
+			var position = $scope.keyIndex
+			console.log($scope.randomIndex)
+			for ( var i = 0; i < $scope.scalesArray[$scope.randomIndex].scalePattern.length; i++ ) {
+				$scope.compareArray.push($scope.keysArray[position])
+				position += $scope.scalesArray[$scope.randomIndex].scalePattern[i]
+			}
+			$scope.compareArray.filter(function(el){
+				$scope.notes.push(el.key);
+			})
+			console.log($scope.notes)
+			$scope.selectTimbreLC = $scope.selectTimbre.toLowerCase()
+			
+			var timbre = new Wad({
+				source  : $scope.selectTimbreLC,
+			})
 
-
-
-
+			if($scope.notes.length === 7){
+		
+				timbre.play({
+					volume  : 0.5, 
+					loop    : false, 
+					pitch   : $scope.notes[0],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+					});
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 0.5,
+					loop    : false, 
+					pitch   : $scope.notes[1],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1,
+					loop    : false, 
+					pitch   : $scope.notes[2],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1.5,
+					loop    : false, 
+					pitch   : $scope.notes[3],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2,
+					loop    : false, 
+					pitch   : $scope.notes[4],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2.5,
+					loop    : false, 
+					pitch   : $scope.notes[5],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 3,
+					loop    : false, 
+					pitch   : $scope.notes[6],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+			}
+			else if($scope.notes.length === 8){
+				timbre.play({
+					volume  : 0.5, 
+					loop    : false, 
+					pitch   : $scope.notes[0],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+					});
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 0.5,
+					loop    : false, 
+					pitch   : $scope.notes[1],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1,
+					loop    : false, 
+					pitch   : $scope.notes[2],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1.5,
+					loop    : false, 
+					pitch   : $scope.notes[3],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2,
+					loop    : false, 
+					pitch   : $scope.notes[4],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2.5,
+					loop    : false, 
+					pitch   : $scope.notes[5],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 3,
+					loop    : false, 
+					pitch   : $scope.notes[6],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 3.5,
+					loop    : false, 
+					pitch   : $scope.notes[7],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+			}
+			else if($scope.notes.length === 12){
+				timbre.play({
+					volume  : 0.5, 
+					loop    : false, 
+					pitch   : $scope.notes[0],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+					});
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 0.5,
+					loop    : false, 
+					pitch   : $scope.notes[1],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1,
+					loop    : false, 
+					pitch   : $scope.notes[2],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 1.5,
+					loop    : false, 
+					pitch   : $scope.notes[3],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2,
+					loop    : false, 
+					pitch   : $scope.notes[4],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 2.5,
+					loop    : false, 
+					pitch   : $scope.notes[5],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 3,
+					loop    : false, 
+					pitch   : $scope.notes[6],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 3.5,
+					loop    : false, 
+					pitch   : $scope.notes[7],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 4,
+					loop    : false, 
+					pitch   : $scope.notes[8],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 4.5,
+					loop    : false, 
+					pitch   : $scope.notes[9],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 5,
+					loop    : false, 
+					pitch   : $scope.notes[10],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+				timbre.play({
+					volume  : 0.5, 
+					wait    : 5.5,
+					loop    : false, 
+					pitch   : $scope.notes[11],
+					detune  : 0,
+					panning : 0,
+					env     : {hold : 0.5},
+				})
+			}
+		}
+		$scope.changeScaleDropdown = function(){
+			if($scope.answerScaleQuiz == $scope.randomIndex){
+				alert("Correct!")
+			}
+			else{
+				alert("you suck!")
+			}
+		}
 
 }
 
